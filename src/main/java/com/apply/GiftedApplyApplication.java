083698a2aa9eec9511d8baf858cca2be93ae6ea8
earlyou <earlyou1@gmail.com>
2022-05-25 19:14:31 +0900

    gifted apply form

---
+++ src/main/java/com/apply/GiftedApplyApplication.java
@@ -0,0 +1,13 @@
+package com.apply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftedApplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftedApplyApplication.class, args);
+	}
+
+}
